d711bb2849c0461f99e3d4d442e0ff2bf4ae09b8
sexi-mod <githubyt135@gmail.com>
2024-06-23 14:03:33 +0530

    Update utubebot.py

--- bot/utubebot.py
+++ bot/utubebot.py
@@ -9,6 +9,7 @@ class UtubeBot(Client):
             name,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
+            bot_token=Config.BOT_TOKEN,
             plugins=dict(root="bot.plugins"),
             workers=6,
         )
